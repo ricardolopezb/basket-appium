afe30215ca74978a5ad080170da7c2ac991d8dc4
ricardolopezb <ricardolopezbonaguro@gmail.com>
2023-05-16 12:55:40 -0300

    changes (?)

--- src/test/java/org/example/BaseIOSTest.java
+++ src/test/java/org/example/BaseIOSTest.java
@@ -1,2 +1,29 @@
-package org.example;public class BaseIOSTest {
+package org.example;
+
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.remote.AutomationName;
+import io.appium.java_client.remote.IOSMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class BaseIOSTest {
+    public static void main(String[] args) throws MalformedURLException {
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "16.4");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 14 Pro Max");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.IOS_XCUI_TEST);
+        capabilities.setCapability(IOSMobileCapabilityType.LAUNCH_TIMEOUT, 500000);
+
+        capabilities.setCapability("commandTimeouts", "12000");
+        capabilities.setCapability(MobileCapabilityType.APP, "/Users/rick/faculty/aseca/appium-ios/src/test/java/org/example/basketmobile.app");
+
+        IOSDriver driver = new IOSDriver(new URL("http://localhost:4723"), capabilities);
+
+
+
+    }
 }
