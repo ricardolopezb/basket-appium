4bb40c12640e0e36eacd2ff288bc1bb239c715cf
ricardolopezb <ricardolopezbonaguro@gmail.com>
2023-06-02 15:22:07 -0300

    Added BaseAndroidTest

---
+++ src/test/java/org/example/BaseAndroidTest.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.remote.AutomationName;
+import io.appium.java_client.remote.IOSMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class BaseAndroidTest {
+    public static AndroidDriver desiredCapabilities() throws MalformedURLException {
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "16.4");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 14 Pro Max");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.IOS_XCUI_TEST);
+        capabilities.setCapability(IOSMobileCapabilityType.LAUNCH_TIMEOUT, 500000);
+
+        capabilities.setCapability("commandTimeouts", "12000");
+        capabilities.setCapability(MobileCapabilityType.APP, "/Users/rick/faculty/aseca/appium-ios/src/test/java/org/example/basketmobile2.app");
+
+        AndroidDriver driver = new AndroidDriver(new URL("http://localhost:4723"), capabilities);
+        return driver;
+
+
+    }
+}
