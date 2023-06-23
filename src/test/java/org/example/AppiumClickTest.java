b65943fc86eacec3af994cc2feb43a65f4a1f6b0
ricardolopezb <ricardolopezbonaguro@gmail.com>
2023-06-22 22:05:09 -0300

    final appium test

--- src/test/java/org/example/AppiumClickTest.java
+++ src/test/java/org/example/AppiumClickTest.java
@@ -1,101 +1,70 @@
 package org.example;
 
 import io.appium.java_client.ios.IOSDriver;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.openqa.selenium.WebElement;
+import org.springframework.util.Assert;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
+import java.net.http.HttpRequest;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Objects;
 
 public class AppiumClickTest extends BaseIOSTest{
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, JSONException, InterruptedException {
         IOSDriver driver = BaseIOSTest.desiredCapabilities();
-        driver.findElementByXPath("(//XCUIElementTypeOther[@name=\"Players\"])[2]").click();
-        driver.findElementByXPath("(//XCUIElementTypeOther[@name=\"Matches\"])[2]").click();
-        driver.findElementByXPath("(//XCUIElementTypeOther[@name=\"Players\"])[2]").click();
-
-        HashMap<String, Object> scrollObject = new HashMap<>();
-        scrollObject.put("direction", "down");
-
-        driver.executeScript("mobile:scroll", scrollObject);
-
-        actualizarScore();
-
-
+        driver.findElementByXPath("//XCUIElementTypeOther[@name=\"match-2c92808388dfcd9e0188dfd3a8300000\"]").click();
+        Thread.sleep(300);
+        String heatMVP = driver.findElementByXPath("//XCUIElementTypeStaticText[@name=\"Jimmy Butler\"]").getText();
+        Assert.isTrue(heatMVP.equals("Jimmy Butler"), "Jimmy Butler is not MVP");
+        driver.findElementByXPath("//XCUIElementTypeButton[@name=\"Back\"]").click();
+        Thread.sleep(300);
+        driver.findElementByXPath("//XCUIElementTypeOther[@name=\"bottombar-barchart2\"]/XCUIElementTypeOther/XCUIElementTypeOther").click();
+        Thread.sleep(300);
+        String standingsTitle = driver.findElementByXPath("//XCUIElementTypeStaticText[@name=\"Standings\"]").getText();
+        Assert.isTrue(standingsTitle.equals("Standings"), "Title is not Standings");
+        Thread.sleep(300);
+        driver.findElementByXPath("//XCUIElementTypeButton[@name=\"Back\"]").click();
 
     }
 
-    private static void actualizarScore() throws IOException {
-        String endpoint = "https://f6a2-181-29-89-79.ngrok-free.app/api/match/"; // Reemplazar con el endpoint real
-        URL url = new URL(endpoint);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("POST");
-
-        String body = "{\"matchId\": \"" + "fdecd8f0-24e6-4241-becf-dd98c70f47bb" + "\","
-                + "\"playerId\": \"" + "5605aa6e-2531-4018-9dc3-9193fd441286" + "\","
-                + "\"points\": " + 1 + ",}";
-        connection.setDoOutput(true);
-
-//        try (DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream())) {
-//            outputStream.writeBytes(body);
-//            outputStream.flush();
-//        }
-        try (DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream())) {
-            byte[] requestBody = body.getBytes(StandardCharsets.UTF_8);
-            outputStream.write(requestBody, 0, requestBody.length);
-        }
-
-        int responseCode = connection.getResponseCode();
-        System.out.println("Respuesta del endpoint: " + responseCode);
-
-        // Leer la respuesta del endpoint si es necesario
-
-        connection.disconnect();
-    }
-
-//    public static void main(String[] args) throws MalformedURLException {
-//        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-//        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-//        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "nombre_del_dispositivo");
-//        desiredCapabilities.setCapability(MobileCapabilityType.APP_PACKAGE, "nombre_del_paquete_de_la_aplicacion");
-//        desiredCapabilities.setCapability(MobileCapabilityType.APP_ACTIVITY, "nombre_de_la_actividad_principal");
-//
-//        AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL("http://localhost:4723/wd/hub"), desiredCapabilities);
-//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//
-//        // Carga de la pantalla en una lista de scores de partidos de basket
-//        AndroidElement scoresList = driver.findElement(By.id("id_de_la_lista"));
-//
-//        // Obtener el número de score antes de la actualización
-//        String scoreAnterior = scoresList.findElement(By.xpath("//android.widget.TextView[1]")).getText();
-//
-//        // Triggeear el endpoint para actualizar el score
-//
-//        // Desplazarse hacia abajo para refreshar la pantalla (pull scroll)
-//        int startX = scoresList.getLocation().getX() + scoresList.getSize().getWidth() / 2;
-//        int startY = scoresList.getLocation().getY() + scoresList.getSize().getHeight() / 2;
-//        int endX = startX;
-//        int endY = startY - 500;
-//
-//        TouchAction touchAction = new TouchAction(driver);
-//        touchAction.press(startX, startY).waitAction(Duration.ofMillis(500)).moveTo(endX, endY).release().perform();
-//
-//        // Esperar a que se cargue el loader o elemento de carga
-//
-//        // Obtener el número de score después de la actualización
-//        String scoreActual = scoresList.findElement(By.xpath("//android.widget.TextView[1]")).getText();
-//
-//        // Verificar que el número de score haya cambiado efectivamente
-//        if (!scoreActual.equals(scoreAnterior)) {
-//            System.out.println("El score se actualizó correctamente");
-//        } else {
-//            System.out.println("El score no se actualizó");
-//        }
-//
-//        driver.quit();
-//    }
 }
+
+
+
+
+
+
+
+
+
+//        driver.findElementByXPath("(//XCUIElementTypeOther[@name=\"Matches\"])[2]").click();
+//        driver.findElementByXPath("(//XCUIElementTypeOther[@name=\"Players\"])[2]").click();
+
+//String createdMatch = driver.findElementByXPath("(//XCUIElementTypeOther[@name="Los Angeles Lakers:0 - Boston Celtics:0"])[2]").getText();
+//        HashMap<String, Object> scrollObject = new HashMap<>();
+//        scrollObject.put("direction", "up");
+//
+//        driver.executeScript("mobile:scroll", scrollObject);
+
+
+//        WebElement upperPickerWheel = driver.findElementByXPath("//XCUIElementTypeOther[@name=\"Select Team 1 Horizontal scroll bar, 1 page Chicago Bulls Boston Celtics Los Angeles Lakers Select Team 1 Horizontal scroll bar, 1 page Golden State Warriors Miami Heat Chicago Bulls Boston Celtics Los Angeles Lakers Select Team 1 Los Angeles Lakers Select Team 1 Horizontal scroll bar, 1 page\"]/XCUIElementTypePicker/XCUIElementTypePickerWheel");
+//        upperPickerWheel.sendKeys("Los Angeles Lakers");
+//
+//        WebElement lowerPickerWheel = driver.findElementByXPath("//XCUIElementTypeOther[@name=\"Select Team 2 Horizontal scroll bar, 1 page Chicago Bulls Boston Celtics Los Angeles Lakers Select Team 2 Horizontal scroll bar, 1 page Golden State Warriors Miami Heat Chicago Bulls Boston Celtics Los Angeles Lakers Select Team 2 Los Angeles Lakers Select Team 2 Horizontal scroll bar, 1 page\"]/XCUIElementTypePicker/XCUIElementTypePickerWheel");
+//        lowerPickerWheel.sendKeys("Boston Celtics");
+//
+//        driver.findElementByXPath("//XCUIElementTypeButton[@name=\"Create Game\"]").click();
+//        driver.findElementByXPath("(//XCUIElementTypeOther[@name=\"ActiveGames\"])[2]").click();
+//
+//        Thread.sleep(300);
+//        String createdMatchText = driver.findElementByXPath("(//XCUIElementTypeOther[@name=\"Los Angeles Lakers:0 - Boston Celtics:0\"])[2]").getText();
+//        System.out.println(createdMatchText);
+//        Assert.notNull(createdMatchText, "Match was not created");
+//        Assert.isTrue(createdMatchText.equals("Los Angeles Lakers:0 - Boston Celtics:0"), "Match was not created");
